b601528d938ea3569b77415a41cdc070c972ecd7
Vasu Garg <vasu.garg@coveidentity.com>
2022-10-04 22:30:53 +0530

    pipe module

--- src/app/components/horoscope/horoscope.module.ts
+++ src/app/components/horoscope/horoscope.module.ts
@@ -7,14 +7,15 @@ import { MatSelectModule } from '@angular/material/select';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { TrimTextPipe } from 'src/app/shared/trim-text/trim-text.pipe';
+import { TrimTextModule } from 'src/app/shared/trim-text/trim-text.module';
 
 @NgModule({
-  declarations: [HoroscopeComponent, TrimTextPipe],
+  declarations: [HoroscopeComponent],
   imports: [
     CommonModule,
     HoroscopeRoutingModule,
     FormsModule,
+    TrimTextModule,
     MatFormFieldModule,
     MatSelectModule,
     MatButtonModule,
--- src/app/components/movie-database/movie-database.module.ts
+++ src/app/components/movie-database/movie-database.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { MovieDatabaseRoutingModule } from './movie-database-routing.module';
 import { MovieDatabaseComponent } from './movie-database.component';
 import { FormsModule } from '@angular/forms';
-import { TrimTextPipe } from 'src/app/shared/trim-text/trim-text.pipe';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -12,13 +11,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { TrimTextModule } from 'src/app/shared/trim-text/trim-text.module';
 
 @NgModule({
-  declarations: [MovieDatabaseComponent, TrimTextPipe],
+  declarations: [MovieDatabaseComponent],
   imports: [
     CommonModule,
     MovieDatabaseRoutingModule,
     FormsModule,
+    TrimTextModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
