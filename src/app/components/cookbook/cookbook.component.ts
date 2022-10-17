4d74c596192f55dfba64258e7b100099927b1243
vasu garg <vasugarg6656@gmail.com>
2022-10-17 23:53:16 +0530

    cookbook

--- src/app/components/cookbook/cookbook.component.ts
+++ src/app/components/cookbook/cookbook.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { INGREDIENTS } from 'src/app/models/cookbook';
+import { CookbookService } from 'src/app/services/cookbook/cookbook.service';
 
 @Component({
   selector: 'app-cookbook',
   templateUrl: './cookbook.component.html',
-  styleUrls: ['./cookbook.component.scss']
+  styleUrls: ['./cookbook.component.scss'],
 })
 export class CookbookComponent implements OnInit {
+  viewModes = {
+    category: 'categories',
+    dishes: 'dishes',
+    recipe: 'recipe',
+  };
+  view = this.viewModes.category;
 
-  constructor() { }
+  ingredients = INGREDIENTS.sort();
+  dishes: any;
 
-  ngOnInit(): void {
+  constructor(private cookbookService: CookbookService) {}
+
+  ngOnInit(): void {}
+
+  getDishes(ingredient: string) {
+    this.cookbookService.searchRecipes(ingredient).subscribe((res) => {
+      this.dishes = res;
+      this.view = this.viewModes.dishes;
+    });
   }
-
 }
