52c1444d9dfecd7f205839e34bcaa7d74c6d3edf
vasu garg <vasugarg6656@gmail.com>
2022-11-14 00:17:02 +0530

    game details css

--- src/app/components/games/game-review/game-review.module.ts
+++ src/app/components/games/game-review/game-review.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { GameReviewRoutingModule } from './game-review-routing.module';
 import { GameReviewComponent } from './game-review.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [GameReviewComponent],
-  imports: [CommonModule, GameReviewRoutingModule, MatProgressSpinnerModule],
+  imports: [
+    CommonModule,
+    GameReviewRoutingModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+  ],
 })
 export class GameReviewModule {}
