07f29938b92ca9f532887c9c80683f4489383af0
vasu garg <vasugarg6656@gmail.com>
2022-11-13 12:50:07 +0530

    movie db routing

--- src/app/components/movie-database/movie-database-routing.module.ts
+++ src/app/components/movie-database/movie-database-routing.module.ts
@@ -2,7 +2,31 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MovieDatabaseComponent } from './movie-database.component';
 
-const routes: Routes = [{ path: '', component: MovieDatabaseComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: MovieDatabaseComponent,
+    children: [
+      {
+        path: 'gallery',
+        loadChildren: () =>
+          import('./gallery/gallery.module').then((m) => m.GalleryModule),
+      },
+      {
+        path: 'review/:movieId',
+        loadChildren: () =>
+          import('./movie-review/movie-review.module').then(
+            (m) => m.MovieReviewModule
+          ),
+      },
+      {
+        path: '',
+        redirectTo: '/movie-db/gallery',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/components/movie-database/movie-database.module.ts
+++ src/app/components/movie-database/movie-database.module.ts
@@ -3,15 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { MovieDatabaseRoutingModule } from './movie-database-routing.module';
 import { MovieDatabaseComponent } from './movie-database.component';
-import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { TrimTextModule } from 'src/app/shared/trim-text/trim-text.module';
 import { TitlebarModule } from 'src/app/shared/titlebar/titlebar.module';
 
 @NgModule({
@@ -19,16 +13,10 @@ import { TitlebarModule } from 'src/app/shared/titlebar/titlebar.module';
   imports: [
     CommonModule,
     MovieDatabaseRoutingModule,
-    FormsModule,
-    TrimTextModule,
-    MatFormFieldModule,
-    MatInputModule,
     MatButtonModule,
     MatIconModule,
-    MatCardModule,
-    MatProgressSpinnerModule,
     MatProgressBarModule,
-    TitlebarModule
+    TitlebarModule,
   ],
 })
-export class MovieDatabaseModule { }
+export class MovieDatabaseModule {}
